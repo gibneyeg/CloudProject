890a34aac6e9edde524110d4930e62c536f3ed1c
Eve Gibney <gibney.eg@gmail.com>
2024-11-12 17:07:44 +0000

    Attempt to fix github actions again

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,7 @@
 require_relative "boot"
-
 require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module LibrarySystem
@@ -13,8 +11,8 @@ module LibrarySystem
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
@@ -24,12 +22,5 @@ module LibrarySystem
         controller_specs: true,
         request_specs: true
     end
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
